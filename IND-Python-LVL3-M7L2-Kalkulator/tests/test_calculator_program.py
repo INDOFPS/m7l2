2f2c57317e0e019531e93516ba69104a77130d77
INDOFPS <ahmaddyasa435@gmail.com>
2026-02-04 20:36:21 +0700

    m7l2 lvl 3

---
+++ IND-Python-LVL3-M7L2-Kalkulator/tests/test_calculator_program.py
@@ -0,0 +1,20 @@
+import pytest
+from calculate.calculator_program import calculate
+
+def test_calculate_addition():
+    assert calculate(1, 1, '+') == 2
+
+def test_calculate_division():
+    assert calculate(8, 2, '/') == 4
+
+def test_calculate_unknown_operation():
+    assert calculate(5, 5, 'tidak diketahui') == 'Operasi tidak dikenal.'
+
+'''
+Tugas. Saat ini ada tiga unit-test yang sudah dibuat
+Program mengecek apakah kalkulator berfungsi dengan benar untuk operasi penjumlahan, pembagian dan operasi yang tidak dikenal
+Kamu perlu menambahkan minimal tes untuk operasi berikut:
+1. Pengurangan
+2. Perkalian
+Akan lebih keren kalau kamu bisa membuat dan menambahkan tes-tes tambahan
+'''
---
+++ IND-PythonLVL3-M7L2-Pembalik-String/reverse_string/string_reverser.py
@@ -0,0 +1,29 @@
+# Fungsi untuk membalikkan string
+def reverse_string(s):
+    # Tugas: Lengkapi fungsi ini
+    reverse_string = ""
+
+    for char in s:
+        reverse_string = char + reverse_string
+    '''
+    Membuat string kosong untuk menyimpan hasil
+    Melakukan iterasi untuk setiap karakter dalam string asli
+    Menambahkan karakter saat ini ke awal string hasil
+    Dengan cara ini, karakter terakhir akan menjadi pertama, kedua terakhir menjadi kedua, dst
+    Mengembalikan string yang sudah dibalik
+    '''
+    return reverse_string
+
+
+# Fungsi utama program
+def main():
+    # Meminta input string dari pengguna untuk dibalik
+    user_input = input("Masukkan string yang ingin dibalik: ")
+    # Memanggil fungsi reverse_string dengan string dari pengguna
+    # Menampilkan string yang sudah dibalik ke layar
+    print("String yang dibalik:", reverse_string(user_input))
+
+
+# Memeriksa apakah program dijalankan secara langsung dan bukan diimpor sebagai modul
+if __name__ == "__main__":
+    main()  # Memanggil fungsi utama jika program dijalankan secara langsung
